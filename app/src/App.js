121616a062c558dcbf1eec670f85fd0671b5dc54
John Bae <john21225@gmail.com>
2021-12-25 18:22:51 -0800

    Data manipulation and bar chart created

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,49 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { csv, max, scaleBand, scaleLinear } from "d3";
 
-function App() {
-	return <div className="App">Application</div>;
-}
+const App = () => {
+	const csvUrl =
+		"https://gist.githubusercontent.com/BonkHo/b3de3a613a954de4af97f8f0c3d35f2c/raw/ea0362c3a3977b876a065f0e1fdc547cb4913455/unitedNationsTotalPopulation.csv";
+
+	const [data, setData] = useState(null);
+
+	const width = 1280;
+	const height = 700;
+
+	useEffect(() => {
+		const row = (d) => {
+			d.Population = +d["2020"];
+			return d;
+		};
+		csv(csvUrl, row).then(setData);
+	}, []);
+
+	if (!data) {
+		return <pre>"Loading"</pre>;
+	}
+
+	const yScale = scaleBand()
+		.domain(data.map((d) => d.Country))
+		.range([0, height]);
+
+	const xScale = scaleLinear()
+		.domain([0, max(data, (d) => d.Population)])
+		.range([0, width]);
+
+	return (
+		<svg width={width} height={height}>
+			{data.map((d) => (
+				<rect
+					x={0}
+					y={yScale(d.Country)}
+					width={xScale(d.Population)}
+					height={yScale.bandwidth()}
+				>
+					{" "}
+				</rect>
+			))}
+		</svg>
+	);
+};
 
 export default App;
