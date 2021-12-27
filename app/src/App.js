5dab139aaf8685613551b5d6ebf27b304563aa76
John Bae <john21225@gmail.com>
2021-12-27 15:06:34 -0800

    Bar chart working properly with labeled axies

--- app/src/App.js
+++ app/src/App.js
@@ -3,15 +3,15 @@ import { csv, max, scaleBand, scaleLinear } from "d3";
 
 const App = () => {
 	const csvUrl =
-		"https://gist.githubusercontent.com/BonkHo/b3de3a613a954de4af97f8f0c3d35f2c/raw/ea0362c3a3977b876a065f0e1fdc547cb4913455/unitedNationsTotalPopulation.csv";
+		"https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
 
 	const [data, setData] = useState(null);
 
 	const width = 1000;
 	const height = 400;
-	const margin = { top: 20, right: 20, bottom: 20, left: 20 };
-	const innerWidth = height - margin.left - margin.right;
-	const innerHeight = width - margin.top - margin.bottom;
+	const margin = { top: 20, right: 20, bottom: 20, left: 200 };
+	const innerWidth = width - margin.left - margin.right;
+	const innerHeight = height - margin.top - margin.bottom;
 
 	useEffect(() => {
 		const row = (d) => {
@@ -41,13 +41,31 @@ const App = () => {
 		<svg width={width} height={height}>
 			<g transform={`translate(${margin.left}, ${margin.top})`}>
 				{xScale.ticks().map((tick) => (
-					<g transform={`translate(${xScale(tick)}, 0)`}>
+					<g key={tick} transform={`translate(${xScale(tick)}, 0)`}>
 						<line y2={innerHeight} stroke="black" />
-						<text style={{ textAnchor: "middle" }}>{tick}</text>
+						<text
+							dy={"1em"}
+							y={innerHeight + 3}
+							style={{ textAnchor: "middle" }}
+						>
+							{tick}
+						</text>
 					</g>
+				))}
+				{yScale.domain().map((domain) => (
+					<text
+						key={domain}
+						dy=".32em"
+						x={-10}
+						y={yScale(domain) + yScale.bandwidth() / 2}
+						style={{ textAnchor: "end" }}
+					>
+						{domain}
+					</text>
 				))}
 				{data.map((d) => (
 					<rect
+						key={d.Country}
 						x={0}
 						y={yScale(d.Country)}
 						width={xScale(d.Population)}
